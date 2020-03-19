cbf66de14a662a630bfd7da204ba92887c98cbb7
Benjamin Leeds <baleeds@ncsu.edu>
2020-03-19 10:16:40 -0400

    add: Options to createMockClient

--- src/mockClient.ts
+++ src/mockClient.ts
@@ -1,29 +1,39 @@
-import ApolloClient from 'apollo-client';
-import { InMemoryCache as Cache, NormalizedCacheObject } from 'apollo-cache-inmemory';
-import { DocumentNode } from 'apollo-link';
-import { MockLink } from './mockLink';
+import ApolloClient, { ApolloClientOptions } from "apollo-client";
+import {
+  InMemoryCache as Cache,
+  NormalizedCacheObject
+} from "apollo-cache-inmemory";
+import { DocumentNode } from "apollo-link";
+import { MockLink } from "./mockLink";
 
-export type RequestHandler<TData = any, TVariables = any> =
-  (variables: TVariables) => Promise<RequestHandlerResponse<TData>>;
+export type RequestHandler<TData = any, TVariables = any> = (
+  variables: TVariables
+) => RequestHandlerResponse<TData>;
 
-export type RequestHandlerResponse<T> = { data: T, errors?: any[] };
+export type RequestHandlerResponse<T> = { data: T; errors?: any[] };
 
-export type MockApolloClient = ApolloClient<NormalizedCacheObject> &
-  { setRequestHandler: (query: DocumentNode, handler: RequestHandler) => void };
+export type MockApolloClient = ApolloClient<NormalizedCacheObject> & {
+  setRequestHandler: (query: DocumentNode, handler: RequestHandler) => void;
+};
 
-export const createMockClient = (): MockApolloClient => {
+export interface MockApolloClientOptions {
+  clientOptions?: Partial<ApolloClientOptions<any>>;
+}
+
+export const createMockClient = (
+  { clientOptions }: MockApolloClientOptions = { clientOptions: {} }
+): MockApolloClient => {
   const mockLink = new MockLink();
 
   const client = new ApolloClient({
-    cache: new Cache({
-      addTypename: false, // TODO: Handle addTypename?
-    }),
-    link: mockLink,
+    cache: new Cache(),
+    ...clientOptions,
+    link: mockLink
   });
 
   const mockMethods = {
-    setRequestHandler: mockLink.setRequestHandler.bind(mockLink),
+    setRequestHandler: mockLink.setRequestHandler.bind(mockLink)
   };
 
   return Object.assign(client, mockMethods);
-}
+};
